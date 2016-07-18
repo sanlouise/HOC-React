9da13011065a51bb082d7c8df348dcba8cdbf6d8
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-07-18 22:57:29 +0200

    Minor fixes

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Header from './header'
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
       <div>
