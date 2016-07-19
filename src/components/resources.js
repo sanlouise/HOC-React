36091779efef0a7a8c198bdd531f61e8b0a148a4
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-07-19 08:55:26 +0200

    Create basic Resources Component

---
+++ src/components/resources.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default () => {
+
+	return (
+		<div>
+			Secret Resources
+			<ul>
+				<li>Secret 1</li>
+				<li>Secret 2</li>
+				<li>Secret 3</li>
+				<li>Secret 4</li>
+			</ul>
+		</div>
+	);
+};
